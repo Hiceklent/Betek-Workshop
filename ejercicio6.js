c46f6b92dda2727d367a6d13bedd0509613639fc
Holguer Pinzon <hiceklent@gmail.com>
2024-05-30 17:06:05 -0500

    Bug arreglado en la validacion y añadimos en consola si es extrangero

--- ejercicio6.js
+++ ejercicio6.js
@@ -3,5 +3,6 @@ let numeroDePasaporte = prompt("Ingrese el primer digito de su pasaporte!").toLo
 let formula = numeroDePasaporte === "a" ? "Toma la primera salida a la izquierda para vuelos nacionales" : "Toma la primera salida a la derecha para vuelos internacionales";
 let formula2 = numeroDePasaporte === "a";
 
-console.log("Usuario extrangero: " + formula2)
+console.log("Usuario Nacional: " + formula2)
+console.log("Usuario Extanjero: " + !formula2)
 console.log(formula);
