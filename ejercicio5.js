5d6b24a79d81818ae73555b5f137628ddd1587d5
Holguer Pinzon <hiceklent@gmail.com>
2024-05-30 15:48:04 -0500

    Ejercicio 5 Realizado

---
+++ ejercicio5.js
@@ -0,0 +1,3 @@
+let pregunta = prompt("Escribe una letra para saber si es una vocal!!");
+let formula = pregunta === "a" || pregunta === "e" || pregunta === "i" || pregunta === "o" || pregunta === "u" ;
+console.log(formula);
