7b717283ae3310bc04653b9326314ee27662ae1b
Holguer Pinzon <hiceklent@gmail.com>
2024-05-30 18:04:40 -0500

    Se Ah Resuelto el ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,18 @@
+let a = 4;
+let b = 7;
+let c = 2 * b;
+let x = a;
+let y = 3 * x;
+let u = 6;
+let w = 10;
+let z = 2 * w;
+
+let resultado1 = a / (b * c);
+let resultado2 = a ** 2 + b ** 92;
+let resultado3 = (x + y) / (u / 1 + w / a );
+let resultado4 = (x / y) * (z + w);
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+console.log(resultado4);
