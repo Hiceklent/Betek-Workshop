4e87d1da9c0f6902186d0951664bbdd32836fae8
Tatiana Garcia <t.dvla8549@gmail.com>
2024-05-30 16:26:45 -0500

    Agregando ejercicio n6

---
+++ ejercicio6.js
@@ -0,0 +1,7 @@
+let numeroDePasaporte = prompt("Ingrese el primer digito de su pasaporte!").toLowerCase();
+
+let formula = numeroDePasaporte === "a" ? "Toma la primera salida a la izquierda para vuelos nacionales" : "Toma la primera salida a la derecha para vuelos internacionales";
+let formula2 = numeroDePasaporte === "a";
+
+console.log("Usuario extrangero: " + formula2)
+console.log(formula);
