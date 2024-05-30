e8813e80e2efbdd43bf277142563b897d834ff87
Tatiana Garcia <t.dvla8549@gmail.com>
2024-05-30 18:43:05 -0500

    Tabla de verdad ejercicio a y b

--- ejercicio8.js
+++ ejercicio8.js
@@ -12,3 +12,33 @@ console.log(!p1);
 console.log(!p2);
 console.log(!p3);
 console.log(!p4);
+
+console.log("Aquí empieza el NOT P AND Q");
+
+
+console.log(!p1 && q1);
+console.log(!p2 && q2);
+console.log(!p3 && q3);
+console.log(!p4 && q4);
+
+
+console.log("Aquí empieza el ejercicio B");
+
+console.log(!p1);
+console.log(!p2);
+console.log(!p3);
+console.log(!p4);
+
+
+console.log(!q1);
+console.log(!q2);
+console.log(!q3);
+console.log(!q4);
+
+
+console.log("Aquí comienza el NOT P AND NOT Q");
+
+console.log(!p1 && !q1);
+console.log(!p2 && !q2);
+console.log(!p3 && !q3);
+console.log(!p4 && !q4);
