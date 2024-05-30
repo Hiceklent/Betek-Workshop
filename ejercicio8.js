37d1dde461125ca59cc139b8b4c7bed3323b8bad
Tatiana Garcia <t.dvla8549@gmail.com>
2024-05-30 18:27:48 -0500

    Ejercicio 8

---
+++ ejercicio8.js
@@ -0,0 +1,14 @@
+let p1 = true;
+let p2 = true;
+let p3 = false;
+let p4 = false;
+
+let q1 = true;
+let q2 = false;
+let q3 = true;
+let q4 = false;
+ 
+console.log(!p1);
+console.log(!p2);
+console.log(!p3);
+console.log(!p4);
