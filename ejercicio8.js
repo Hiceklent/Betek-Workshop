746b729f95ae2fa5d208661b501201fe13abc454
Tatiana Garcia <t.dvla8549@gmail.com>
2024-06-04 11:40:19 -0500

    Guardando cambios de ejercicio 8

--- ejercicio8.js
+++ ejercicio8.js
@@ -7,7 +7,9 @@ let q1 = true;
 let q2 = false;
 let q3 = true;
 let q4 = false;
- 
+
+console.log("Aquí es NOT P");
+
 console.log(!p1);
 console.log(!p2);
 console.log(!p3);
@@ -24,11 +26,14 @@ console.log(!p4 && q4);
 
 console.log("Aquí empieza el ejercicio B");
 
+console.log("Aquí es NOT P");
+
 console.log(!p1);
 console.log(!p2);
 console.log(!p3);
 console.log(!p4);
 
+console.log("Aquí es NOT Q");
 
 console.log(!q1);
 console.log(!q2);
@@ -36,7 +41,9 @@ console.log(!q3);
 console.log(!q4);
 
 
+
 console.log("Aquí comienza el NOT P AND NOT Q");
+
 
 console.log(!p1 && !q1);
 console.log(!p2 && !q2);
